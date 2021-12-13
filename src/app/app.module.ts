a4f91746b174771ee17286d9e53b22d34408609c
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-12-13 01:38:55 -0300

    Apresentação finalizada.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ApresentacaoComponent
   ],
   imports: [
     BrowserModule,
